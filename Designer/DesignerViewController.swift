2dcd625159aa76fa02f573580a07fdd4fdbbb0a3
xiaoP <anddygong@gmail.com>
2017-02-25 00:46:11 +0800

    .

---
+++ Designer/DesignerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DesignerViewController.swift
+//  Designer
+//
+//  Created by xiaoP on 2017/2/25.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+class DesignerViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+    
+}
---
+++ Designer/Target_Designer.swift
@@ -0,0 +1,19 @@
+//
+//  Target_Designer.swift
+//  Designer
+//
+//  Created by xiaoP on 2017/2/25.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+@objc (Target_Designer)
+class Target_Designer: NSObject {
+    
+    func Action_designerViewController() -> UIViewController {
+        let vc = DesignerViewController()
+        return vc
+    }
+    
+}
